a67b8940ddb88a556e166dc1734e47c960f7c7f4
Louis Chan <lchan@launchdarkly.com>
2022-05-18 17:20:01 -0700

    With customer's initialization

--- ManualTestApp/App.js
+++ ManualTestApp/App.js
@@ -1,173 +1,80 @@
-import React, { useState, useEffect } from 'react';
-import { SafeAreaView,
-         ScrollView,
-         StyleSheet,
-         Text,
-         View,
-         Button,
-         TextInput,
-         Alert,
-         Switch,
-       } from 'react-native';
-import { Picker } from '@react-native-picker/picker';
+import React, {Component} from 'react';
+import {Text} from 'react-native';
 import LDClient from 'launchdarkly-react-native-client-sdk';
 
-const Wrapper = ({children}): Node => {
-  const styles = {
-    scroll: { backgroundColor: '#fff', padding: 10 },
-    area: { backgroundColor: '#fff', flex: 1 },
-  };
-  return (
-    <SafeAreaView style={styles.area}>
-      <ScrollView style={styles.scroll}>
-        {children}
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
+class App extends Component {
+  state = {output: 'default string'};
 
-const Body = () => {
-  const [client, setClient] = useState(null);
-  const [flagKey, setFlagKey] = useState('flag-key');
-  const [flagType, setFlagType] = useState('bool');
-  const [isOffline, setIsOffline] = useState(false);
-  const [userKey, setUserKey] = useState('user key');
-  const [listenerKey, setListenerKey] = useState('');
-  const [listeners, setListeners] = useState({});
+  init(userConfig) {
+    const clientConfig = {
+      mobileKey: 'MOBILE_KEY',
+      offline: false,
+      // https://github.com/mitmproxy/mitmproxy/issues/4469
+      // BrowserStack is using this as proxy and until the issue above is resolved we would
+      // need to keep Server Side Events off in order to have a working Network Inspection monitor
+      // --------------------------------------------------------------------
+      // An incompatibility between react-native@0.66 and launch-darkly@5.1.0
+      // has caused us to use stream:false on android devices
+      // https://advanceautoparts.atlassian.net/browse/MOB-5449
+      stream: true,
+      evaluationReasons: true,
+    };
+    this.client = new LDClient();
+    this.isCurrentlyInitialising = Promise.race([
+      this.client.configure(clientConfig, userConfig),
+      new Promise(resolve => {
+        setTimeout(() => {
+          resolve(null);
+        }, 1000);
+      }),
+    ]);
+    return this.isCurrentlyInitialising.then(() => {
+      this.isCurrentlyInitialising = undefined;
+    });
+  }
 
-  useEffect(() => {
-    async function initializeClient() {
-      let ldClient = new LDClient();
-      let config = {
-        mobileKey: 'MOBILE_KEY',
-        debugMode: true,
+  async configure() {
+    console.info('LaunchDarkly: Began configuration');
+    try {
+      const userConfig = {
+        key: 'default',
+        anonymous: true,
       };
-      let user = { key: userKey };
-      try {
-        await ldClient.configure(config, user);
-      } catch (err) {
-        console.error(err);
+
+      if (this.isCurrentlyInitialising) {
+        await this.isCurrentlyInitialising;
       }
-      setClient(ldClient);
+
+      if (this.client) {
+        await this.currentlyIdentifyingPromise;
+        this.currentlyIdentifyingPromise = this.client.identify(userConfig);
+        await this.currentlyIdentifyingPromise;
+      } else {
+        await this.init(userConfig);
+      }
+      console.info('LaunchDarkly: Finished configuration');
+    } catch (error) {
+      console.error('LaunchDarkly: Could not configure', error);
+      throw error;
     }
 
-    if (client == null) {
-      initializeClient();
-    }
-  });
-
-  const evalFlag = async () => {
-    let res;
-    if (flagType === 'bool') {
-      res = await client.boolVariation(flagKey, false);
-    } else if (flagType === 'string') {
-      res = await client.stringVariation(flagKey, '');
-    } else if (flagType === 'int') {
-      res = await client.intVariation(flagKey, 0);
-    } else if (flagType === 'float') {
-      res = await client.floatVariation(flagKey, 0.0);
-    } else if (flagType === 'array') {
-      res = await client.jsonVariation(flagKey, []);
-    } else if (flagType === 'object') {
-      res = await client.jsonVariation(flagKey, {});
-    }
-
-    Alert.alert('LD Server Response', JSON.stringify(res));
+    return 'what';
   }
 
-  const track = () => {
-    client.track(flagKey, false);
+  async getFlag() {
+    let result = this.configure().then(
+      this.client.stringVariation('string-flag', 'hey').then(value => {
+        console.log(value);
+        this.setState = {output: JSON.stringify(value)};
+      }),
+    );
+    return await result;
   }
 
-  const identify = () => {
-    client.identify({ key: userKey });
+  render() {
+    this.getFlag();
+    return <Text>FlagValue "{this.state.output}"</Text>;
   }
-
-  const listen = () => {
-    if (listeners.hasOwnProperty(listenerKey)) {
-      return;
-    }
-    let listener = value => Alert.alert('Listener Callback', value);
-    client.registerFeatureFlagListener(listenerKey, listener);
-    setListeners({ ...listeners, ...{[listenerKey]: listener}});
-  }
-
-  const removeListener = () => {
-    client.unregisterFeatureFlagListener(listenerKey, listeners[listenerKey]);
-    let {[listenerKey]: omit, ...newListeners} = listeners;
-    setListeners(newListeners);
-  }
-
-  const flush = () => { client.flush(); }
-
-  const setOffline = (newIsOffline) => {
-    if (newIsOffline) {
-      client.setOffline();
-    } else {
-      client.setOnline();
-    }
-    setIsOffline(newIsOffline);
-  }
-
-  return (
-    <>
-      <Text>Feature Key:</Text>
-      <TextInput style={styles.input}
-                 onChangeText={setFlagKey} value={flagKey} />
-      <View style={styles.row}>
-        <Button title="Evaluate Flag" onPress={evalFlag} />
-        <Picker style={{flex: 1}}
-                selectedValue={flagType}
-                onValueChange={(itemValue, _) => setFlagType(itemValue)}>
-          <Picker.Item label="Bool" value="bool" />
-          <Picker.Item label="String" value="string" />
-          <Picker.Item label="Int" value="int" />
-          <Picker.Item label="Float" value="float" />
-          <Picker.Item label="Array" value="array" />
-          <Picker.Item label="Object" value="object" />
-        </Picker>
-        <Text>Offline</Text>
-        <Switch value={isOffline} onValueChange={setOffline} />
-      </View>
-      <Text>User Key:</Text>
-      <TextInput style={styles.input}
-                 onChangeText={setUserKey} value={userKey} />
-      <View style={styles.row}>
-        <Button title="Identify" onPress={identify} />
-        <Button title="Track" onPress={track} />
-        <Button title="Flush" onPress={flush} />
-      </View>
-      <Text>Feature Flag Listener Key:</Text>
-      <TextInput style={styles.input}
-                 onChangeText={setListenerKey}
-                 value={listenerKey} />
-      <View style={styles.row}>
-        <Button title="Listen" onPress={listen}/>
-        <Button title="Remove" onPress={removeListener}/>
-      </View>
-    </>
-  );
-}
-
-const styles = StyleSheet.create({
-  row: {
-    flexDirection: 'row',
-    paddingVertical: 10,
-    alignItems: 'center',
-  },
-  input: {
-    height: 40,
-    borderColor: 'gray',
-    borderWidth: 1,
-  },
-});
-
-const App = () => {
-  return (
-    <Wrapper>
-      <Body />
-    </Wrapper>
-  );
 }
 
 export default App;
